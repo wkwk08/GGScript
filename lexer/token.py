0ae0ce192edf5ec6907a05acbaa544722d501efa
wkwk08 <jerichoambrocio123@gmail.com>
2025-11-09 08:04:57 +0800

    refactor(token): simplify

--- lexer/token.py
+++ lexer/token.py
@@ -1,19 +1,8 @@
 from .token_types import TokenType
 
 class Token:
-    """
-    Represents a token in the GGScript language
-    """
-    def __init__(self, type, value, line, column):
-        self.type = type      # TokenType enum
-        self.value = value    # Actual value (string, int, float, etc.)
-        self.line = line      # Line number in source
-        self.column = column  # Column number in source
-    
-    def __repr__(self):
-        """String representation for debugging"""
-        return f"Token({self.type.name}, '{self.value}', Line: {self.line}, Col: {self.column})"
-    
-    def __str__(self):
-        """User-friendly string representation"""
-        return f"{self.type.name}: {self.value}"
+    def __init__(self, type_, value=None, line=1, column=1):
+        self.type = type_
+        self.value = value
+        self.line = line
+        self.column = column
