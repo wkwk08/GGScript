4b6c8317ad3f4242f948d636cd0e98988b4b59a2
wkwk08 <jerichoambrocio123@gmail.com>
2025-11-18 06:24:23 +0800

    fix(lexer): correct delimiters

--- lexer/lexer.py
+++ lexer/lexer.py
@@ -44,15 +44,15 @@ PRN_DLM = WHTSPC_DLM + '('
 SEMI_DLM = ';' + WHTSPC_DLM
 
 # Group delimiters
-DATATYPE_DLM = WHTSPC_DLM  # dodge, elo, frag, ign, surebol, stun, tag
-COND_DLM = PRN_BRC_DLM  # choke clutch, clutch, pick
-LOOP_FUNC_DLM = PRN_BLCK_DLM  # build, grind, lobby, retry
-JUMP_DLM = SEMI_DLM  # afk, ggwp, hop
-BOOL_DLM = CMPLX_DLM  # buff, nerf
-DO_ELSE_DLM = BRC_DLM  # choke, try
-IO_ARRAY_DLM = PRN_DLM  # comsat, craft, drop, shout, stack
-METHOD_DLM = DOT_DLM  # count, split
-CASE_DLM = COLON_DLM  # noob, role
+DATATYPE_DLM = WHTSPC_DLM              # frag, elo, ign, surebol, tag, stun
+COND_DLM = WHTSPC_DLM + '({'           # clutch, choke clutch, pick, dodge
+LOOP_FUNC_DLM = WHTSPC_DLM + '({'      # build, grind, lobby, retry
+JUMP_DLM = SEMI_DLM                    # afk, ggwp, hop
+BOOL_DLM = CMPLX_DLM                   # buff, nerf
+DO_ELSE_DLM = WHTSPC_DLM + '{'         # choke, try
+IO_ARRAY_DLM = WHTSPC_DLM + '('        # comsat, craft, drop, shout, stack
+METHOD_DLM = WHTSPC_DLM + '.'          # count, split
+CASE_DLM = WHTSPC_DLM + ':'            # noob, role
 SYMBOL_DLM = OPRTR_DLM # plus, minus, mul, div, mod, assign, lt, gt, eq, neq, and, or
 QUOTE_DLM = STRG_DLM # string and char literals
 SEMI_SYM_DLM = TERMI_DLM # for semicolon and symbols
