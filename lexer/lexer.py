60c84fcb44b92c93992b0eac90694c18602f215f
wkwk08 <jerichoambrocio123@gmail.com>
2025-11-20 22:23:35 +0800

    fix(lexer): clean up delimiter check

--- lexer/lexer.py
+++ lexer/lexer.py
@@ -248,7 +248,7 @@ class Lexer:
         if self.current_char == '+':
             self.advance()
             # Check delimiter after '++'
-            if self.current_char in CMPLX_DLM + CLBRCKT_DLM + WHTSPC_DLM or self.current_char is None:
+            if self.current_char in CMPLX_DLM + WHTSPC_DLM or self.current_char is None:
                 tokens.append(Token(TokenType.increment, '++', start_pos.ln, start_pos.col))
             else:
                 errors.append(LexicalError(start_pos, f"Invalid delimiter '{self.current_char}' after '++'"))
