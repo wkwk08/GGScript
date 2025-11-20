18e661f3a177b7993280685eeed1401bd2175eee
wkwk08 <jerichoambrocio123@gmail.com>
2025-11-20 21:10:55 +0800

    fix(lexer): allow alphanum after '{'

--- lexer/lexer.py
+++ lexer/lexer.py
@@ -1462,7 +1462,7 @@ class Lexer:
     def make_lbrace(self, tokens, errors):
         start_pos = self.pos.copy()
         self.advance()  # {
-        if self.current_char is None or self.current_char in OPBRCKT_DLM:
+        if self.current_char is None or self.current_char in WHTSPC_DLM + ALPHANUM + '{':
             tokens.append(Token(TokenType.bracket, '{', start_pos.ln, start_pos.col))
         else:
             errors.append(LexicalError(start_pos, f"Invalid delimiter '{self.current_char}' after '{{'"))
