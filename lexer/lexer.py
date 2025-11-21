988896405b15b6745d8c395a6701dc30d1cf9076
wkwk08 <jerichoambrocio123@gmail.com>
2025-11-21 22:22:38 +0800

    fix(lexer): update > & >= delimiter checks

--- lexer/lexer.py
+++ lexer/lexer.py
@@ -1319,7 +1319,7 @@ class Lexer:
         self.advance()  # >
         if self.current_char == '=':
             self.advance()
-            if self.current_char is None or self.current_char in SYMBOL_DLM:
+            if self.current_char is None or self.current_char in OPRTR_DLM:
                 tokens.append(Token(TokenType.gte, '>=', start_pos.ln, start_pos.col))
             else:
                 errors.append(LexicalError(start_pos, f"Invalid delimiter '{self.current_char}' after '>='"))
