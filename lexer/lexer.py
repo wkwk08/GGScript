da395f8e57ebfb94962cf89a7be0b4ea0e234d82
wkwk08 <jerichoambrocio123@gmail.com>
2025-12-01 20:31:44 +0800

    fix(lexer): quoted string output

--- lexer/lexer.py
+++ lexer/lexer.py
@@ -1136,7 +1136,7 @@ class Lexer:
         if self.current_char == '"':
             self.advance()
             if self.current_char is None or self.current_char in STRG_DLM:
-                tokens.append(Token(TokenType.string, string_value, start_pos.ln, start_pos.col))
+                tokens.append(Token(TokenType.string, f'"{string_value}"', start_pos.ln, start_pos.col))
             else:
                 errors.append(LexicalError(start_pos, f"Invalid delimiter '{self.current_char}' after string"))
         else:
