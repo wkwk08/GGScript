2db92bda7d0a2cc03a868fe3f9086a5982ac93e0
wkwk08 <jerichoambrocio123@gmail.com>
2026-01-08 02:28:20 +0800

    fix(lexer): update comma handling to use OPRTR_DLM

--- lexer/lexer.py
+++ lexer/lexer.py
@@ -1388,7 +1388,7 @@ class Lexer:
     def make_comma(self, tokens, errors):
         start_pos = self.pos.copy()
         self.advance()  # ,
-        if self.current_char is None or self.current_char in CMPLX_DLM or self.current_char in NUM:
+        if self.current_char is None or self.current_char in OPRTR_DLM:
             tokens.append(Token(TokenType.separator, ',', start_pos.ln, start_pos.col))
         else:
             errors.append(LexicalError(start_pos, f"Invalid delimiter '{self.current_char}' after ','"))
