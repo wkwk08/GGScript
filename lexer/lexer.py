4c861922f5586f975f058b0fc271e605cea50ff6
wkwk08 <jerichoambrocio123@gmail.com>
2026-01-08 04:07:56 +0800

    fix: use CLBRCKT_DLM in make_rbracket

--- lexer/lexer.py
+++ lexer/lexer.py
@@ -1482,7 +1482,7 @@ class Lexer:
     def make_rbracket(self, tokens, errors):
         start_pos = self.pos.copy()
         self.advance()  # ]
-        if self.current_char is None or self.current_char in WHTSPC_DLM + ALPHANUM + ')]':
+        if self.current_char is None or self.current_char in CLBRCKT_DLM:
             tokens.append(Token(TokenType.bracket, ']', start_pos.ln, start_pos.col))
         else:
             errors.append(LexicalError(start_pos, f"Invalid delimiter '{self.current_char}' after ']'"))
