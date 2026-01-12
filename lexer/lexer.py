ef67858337bb4db5f7afae52ef9ccdd6d15e347a
wkwk08 <jerichoambrocio123@gmail.com>
2026-01-13 02:39:35 +0800

    fix: check WHTSPC_DLM instead of DATATYPE_DLM for frag keyword

--- lexer/lexer.py
+++ lexer/lexer.py
@@ -545,7 +545,7 @@ class Lexer:
                         ident_str += self.current_char
                         previous_char = self.current_char
                         self.advance()
-                        if self.current_char is None or self.current_char in DATATYPE_DLM:
+                        if self.current_char is None or self.current_char in WHTSPC_DLM:
                             tokens.append(Token(TokenType.frag, ident_str, start_pos.ln, start_pos.col))
                             matched = True
         elif self.current_char == 'g':
