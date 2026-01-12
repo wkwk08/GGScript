aa67d376295584e88bc3860cfeb78a57a89c2358
wkwk08 <jerichoambrocio123@gmail.com>
2026-01-13 03:29:17 +0800

    fix: check PRN_DLM instead of OP_PRN_DLM for craft keyword

--- lexer/lexer.py
+++ lexer/lexer.py
@@ -473,7 +473,7 @@ class Lexer:
                             ident_str += self.current_char
                             previous_char = self.current_char
                             self.advance()
-                            if self.current_char is None or self.current_char in OP_PRN_DLM:
+                            if self.current_char is None or self.current_char in PRN_DLM:
                                 tokens.append(Token(TokenType.craft, ident_str, start_pos.ln, start_pos.col))
                                 matched = True
         elif self.current_char == 'd':
