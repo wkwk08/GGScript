69bcc1e6947831f6bb249058ddbb5c21a9850da1
wkwk08 <jerichoambrocio123@gmail.com>
2026-01-13 03:01:27 +0800

    fix: check SEMI_DLM instead of JUMP_DLM for hop keyword

--- lexer/lexer.py
+++ lexer/lexer.py
@@ -598,7 +598,7 @@ class Lexer:
                     ident_str += self.current_char
                     previous_char = self.current_char
                     self.advance()
-                    if self.current_char is None or self.current_char in JUMP_DLM:
+                    if self.current_char is None or self.current_char in SEMI_DLM:
                         tokens.append(Token(TokenType.hop, ident_str, start_pos.ln, start_pos.col))
                         matched = True
         elif self.current_char == 'i':
