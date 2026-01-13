2213e484e002f017dc1ef529894143d5bfa2e1cf
wkwk08 <jerichoambrocio123@gmail.com>
2026-01-13 18:18:52 +0800

    fix: use OP_PAREN_DLM for '!' check

--- lexer/lexer.py
+++ lexer/lexer.py
@@ -1417,7 +1417,7 @@ class Lexer:
             else:
                 errors.append(LexicalError(start_pos, f"Invalid delimiter '{self.current_char}' after '!='"))
         else:
-            if self.current_char is None or self.current_char in OPRTR_DLM:
+            if self.current_char is None or self.current_char in OP_PAREN_DLM:
                 tokens.append(Token(TokenType.not_, '!', start_pos.ln, start_pos.col))
             else:
                 errors.append(LexicalError(start_pos, f"Invalid delimiter '{self.current_char}' after '!'"))
