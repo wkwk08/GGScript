8c77e3b9365f3b1e2252d79973e0569dbc64a350
wkwk08 <jerichoambrocio123@gmail.com>
2026-01-14 01:19:49 +0800

    fix: use PRN_DLM for 'count' keyword checks

--- lexer/lexer.py
+++ lexer/lexer.py
@@ -463,7 +463,7 @@ class Lexer:
                             ident_str += self.current_char
                             previous_char = self.current_char
                             self.advance()
-                            if self.current_char is None or self.current_char in DOT_DLM:
+                            if self.current_char is None or self.current_char in PRN_DLM:
                                 tokens.append(Token(TokenType.count, ident_str, start_pos.ln, start_pos.col))
                                 matched = True
             elif self.current_char == 'r':
