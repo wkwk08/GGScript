ec1ae5732916785bd5646138403efc6036f6632f
wkwk08 <jerichoambrocio123@gmail.com>
2026-01-14 00:43:51 +0800

    fix: use REL_OP_DLM for '!=' checks

--- lexer/lexer.py
+++ lexer/lexer.py
@@ -1382,7 +1382,7 @@ class Lexer:
         self.advance()  # !
         if self.current_char == '=':
             self.advance()
-            if self.current_char is None or self.current_char in OPRTR_DLM:
+            if self.current_char is None or self.current_char in REL_OP_DLM:
                 tokens.append(Token(TokenType.neq, '!=', start_pos.ln, start_pos.col))
             else:
                 errors.append(LexicalError(start_pos, f"Invalid delimiter '{self.current_char}' after '!='"))
