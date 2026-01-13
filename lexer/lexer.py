f6b2aabc7d4a2645509fd6b7781b279e428d0c56
wkwk08 <jerichoambrocio123@gmail.com>
2026-01-14 00:42:11 +0800

    fix: use REL_OP_DLM for '/=' checks

--- lexer/lexer.py
+++ lexer/lexer.py
@@ -1273,7 +1273,7 @@ class Lexer:
         elif self.current_char == '=':
             # Division assignment /=
             self.advance()
-            if self.current_char is None or self.current_char in OPRTR_DLM:
+            if self.current_char is None or self.current_char in REL_OP_DLM:
                 tokens.append(Token(TokenType.div_assign, '/=', start_pos.ln, start_pos.col))
             else:
                 errors.append(LexicalError(start_pos, f"Invalid delimiter '{self.current_char}' after '/='"))
