dc257b3f682d7f4f90c19777aa79e894dd1f153f
wkwk08 <jerichoambrocio123@gmail.com>
2026-01-14 00:40:40 +0800

    fix: use REL_OP_DLM for '-=' checks

--- lexer/lexer.py
+++ lexer/lexer.py
@@ -274,7 +274,7 @@ class Lexer:
                 errors.append(LexicalError(start_pos, f"Invalid delimiter '{self.current_char}' after '--'"))
         elif self.current_char == '=':  # minus-assign
             self.advance()
-            if self.current_char is None or self.current_char in OPRTR_DLM:
+            if self.current_char is None or self.current_char in REL_OP_DLM:
                 tokens.append(Token(TokenType.minus_assign, '-=', start_pos.ln, start_pos.col))
             else:
                 errors.append(LexicalError(start_pos, f"Invalid delimiter '{self.current_char}' after '-='"))
