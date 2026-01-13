3cef9aa9e213bbc62e4f255b730706cc794722c6
wkwk08 <jerichoambrocio123@gmail.com>
2026-01-14 00:50:27 +0800

    fix: use WHTSPC_DLM for ';' checks

--- lexer/lexer.py
+++ lexer/lexer.py
@@ -1437,7 +1437,7 @@ class Lexer:
     def make_semicolon(self, tokens, errors):
         start_pos = self.pos.copy()
         self.advance()  # ;
-        if self.current_char is None or self.current_char in TERMI_DLM:
+        if self.current_char is None or self.current_char in WHTSPC_DLM:
             tokens.append(Token(TokenType.terminator, ';', start_pos.ln, start_pos.col))
         else:
             errors.append(LexicalError(start_pos, f"Invalid delimiter '{self.current_char}' after ';'"))
