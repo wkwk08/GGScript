4105105e8cdc2ec605d4ace78ea1370544cb738a
wkwk08 <jerichoambrocio123@gmail.com>
2026-01-14 00:38:45 +0800

    fix: use REL_OP_DLM for '<' and '<=' checks

--- lexer/lexer.py
+++ lexer/lexer.py
@@ -1352,12 +1352,12 @@ class Lexer:
 
         if self.current_char == '=':  # <=
             self.advance()
-            if self.current_char is None or self.current_char in OPRTR_DLM:
+            if self.current_char is None or self.current_char in REL_OP_DLM:
                 tokens.append(Token(TokenType.lte, '<=', start_pos.ln, start_pos.col))
             else:
                 errors.append(LexicalError(start_pos, f"Invalid delimiter '{self.current_char}' after '<='"))
         else:  # <
-            if self.current_char is None or self.current_char in OPRTR_DLM:
+            if self.current_char is None or self.current_char in REL_OP_DLM:
                 tokens.append(Token(TokenType.lt, '<', start_pos.ln, start_pos.col))
             else:
                 errors.append(LexicalError(start_pos, f"Invalid delimiter '{self.current_char}' after '<'"))
